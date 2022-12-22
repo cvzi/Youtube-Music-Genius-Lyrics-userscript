6820cd48f96c094337d7919495fb1ca1b49acd02
cvzi <cuzi@openmail.cc>
2022-12-22 18:39:12 +0100

    Unpin version of GeniusLyrics.js

--- YoutubeMusicGeniusLyrics.user.js
+++ YoutubeMusicGeniusLyrics.user.js
@@ -13,8 +13,8 @@
 // @author          cuzi
 // @icon            https://music.youtube.com/img/favicon_144.png
 // @supportURL      https://github.com/cvzi/Youtube-Music-Genius-Lyrics-userscript/issues
-// @version         4.0.10
-// @require https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js?version=1129650
+// @version         4.0.11
+// @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js
 // @grant           GM.xmlHttpRequest
 // @grant           GM.setValue
 // @grant           GM.getValue
