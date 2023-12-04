399cb0b6238aebf39c7e2e7c67bfc15a342d777c
cvzi <cuzi@openmail.cc>
2023-12-04 16:49:36 +0100

    Version bump to update GeniusLyrics.js

--- YoutubeMusicGeniusLyrics.user.js
+++ YoutubeMusicGeniusLyrics.user.js
@@ -13,7 +13,7 @@
 // @author          cuzi
 // @icon            https://music.youtube.com/img/favicon_144.png
 // @supportURL      https://github.com/cvzi/Youtube-Music-Genius-Lyrics-userscript/issues
-// @version         4.0.21
+// @version         4.0.22
 // @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js
 // @grant           GM.xmlHttpRequest
 // @grant           GM.setValue
